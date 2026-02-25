7c2b07feed5dac0447e754925759c72aa2d24acc
UltimitWinner <57425571+UltimitWinner@users.noreply.github.com>
2026-02-25 10:54:17 -0700

    Basic working code

--- mainXC16.c
+++ mainXC16.c
@@ -37,7 +37,7 @@ void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void){
     _T1IF = 0;
     _T1IE = 0;
     _OC2IE = 1;
-    time ++;
+    time = 1;
 //    if (steps >= 425){
 //        if (turning == 1){
 //            if (fullTurn = 1){
@@ -70,9 +70,9 @@ void stop();
 
 int main(void){
     //setup
-    _RCDIV = 0;
-    ANSA = 0;
-    ANSB = 0;
+    _RCDIV = 0; //set postscailer to 1
+    ANSA = 0; //set to digital logic
+    ANSB = 0; //set to digital logic
     _TRISA1 = 0; //pin 3
     _TRISB2 = 0; //pin 6
     OC2CON1 = 0x1C06; //sets clock and edge alignment
@@ -98,6 +98,16 @@ int main(void){
     
 
     state = Milestone5;
+    milestone5 = Forward1;
+    
+    //initial conditions
+    leftVel = 2000;
+    rightVel = 2000;
+    leftDir = 0;
+    rightDir = 0;
+    drive();
+
+    //main loop
     while (1){
         switch(state){
             case Line:
@@ -145,82 +155,83 @@ int main(void){
             case Milestone5:
                 switch(milestone5){
                     case(Forward1):
-                        leftVel = 2000;
-                        rightVel = 2000;
-                        leftDir = 0;
-                        rightDir = 0;
-                        drive();
-                        if(time>=1){
-                            milestone5 = Turn90;
+                        if(time == 1){
                             time = 0;
                             steps = 0;
+                            TMR1 = 0;
+                            milestone5 = Turn90;
+                            leftVel = 0;
+                            rightVel = 2000;
+                            leftDir = 0;
+                            rightDir = 0;
+                            drive();
                         }
                         break;
                     case(Turn90):
-                        leftVel = 2000;
-                        rightVel = 2000;
-                        leftDir = 1;
-                        rightDir = 0;
-                        drive();
-                        if(steps>=850){
+                        if(steps >= 550){
                             milestone5 = Forward2;
                             time = 0;
                             steps = 0;
+                            TMR1 = 0;
                             _OC2IE = 0;
                             _T1IE = 1;
+                            leftVel = 2000;
+                            rightVel = 2000;
+                            leftDir = 0;
+                            rightDir = 0;
+                            drive();
                         }
                         break;
                     case(Forward2):
-                        leftVel = 2000;
-                        rightVel = 2000;
-                        leftDir = 0;
-                        rightDir = 0;
-                        drive();
                         if(time>=1){
-                            milestone5 = Turn180;
                             time = 0;
                             steps = 0;
+                            TMR1 = 0;
+                            milestone5 = Turn180;
+                            leftVel = 0;
+                            rightVel = 2000;
+                            leftDir = 0;
+                            rightDir = 0;
+                            drive();
                         }
                         break;
                     case(Turn180):
-                        leftVel = 2000;
-                        rightVel = 2000;
-                        leftDir = 1;
-                        rightDir = 0;
-                        drive();
-                        if(steps>=1700){
-                            milestone5 = Forward3;
+                        if(steps>=1100){
                             time = 0;
                             steps = 0;
+                            TMR1 = 0;
                             _OC2IE = 0;
+                            _T1IF = 0;
                             _T1IE = 1;
+                            milestone5 = Forward3;
+                            leftVel = 2000;
+                            rightVel = 2000;
+                            leftDir = 0;
+                            rightDir = 0;
+                            drive();
                         }
                         break;
                     case(Forward3):
-                        leftVel = 2000;
-                        rightVel = 2000;
-                        leftDir = 0;
-                        rightDir = 0;
-                        drive();
-                        if(time>=1){
+                        if(time==1){
                             milestone5 = Finished;
                             time = 0;
                             steps = 0;
+                            leftVel = 0;
+                            rightVel = 0;
+                            leftDir = 0;
+                            rightDir = 0;
+                            time = 0;
+                            steps = 0;
+                            _T1IE = 0;
+                            _OC2IE = 0;
+                            stop();
                         }
                         break;
                     case(Finished):
-                        leftVel = 0;
-                        rightVel = 0;
-                        leftDir = 0;
-                        rightDir = 0;
-                        time = 0;
-                        steps = 0;
-                        _T1IE = 0;
-                        _OC2IE = 0;
                         stop();
                         break;
                     default:
-                        milestone5 = Forward1;
+                        milestone5 = Turn90;
                 }
                 break;
             default:
@@ -240,10 +251,10 @@ void sing(){
 }
 
 void drive(){
-    _LATA1 = leftDir;
-    _LATB2 = rightDir;
-    OC2RS = leftVel;
-    OC1RS = rightVel;
+    _LATB2 = leftDir;
+    _LATA1 = rightDir;
+    OC1RS = leftVel;
+    OC2RS = rightVel;
     OC2R = 50;
     OC1R = 50;
 }
